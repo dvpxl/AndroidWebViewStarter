b59cd631cbe8d19ecee0f05b9860a2477806198c
Jonathan <jonathan@dvpxl.com>
2016-07-31 23:49:31 -0700

    init commit

---
+++ app/src/main/java/com/dvpxl/webviewstarter/MainActivity.java
@@ -0,0 +1,22 @@
+package com.dvpxl.webviewstarter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://yahoo.com");
+
+    }
+}
+
